92eb66a851c2162b691b75996f59e3efe30e841d
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-05-19 16:12:48 -0400

    Fix previous functions added and add last function to convert full number

--- script.js
+++ script.js
@@ -19,85 +19,113 @@ const checkNumber = () => {
 };
 
 const convertToRomanNumeral = (num) => {
-  
+  const romanNumeralArr = num.toString().split("");
+  let convertedNumber;
+  if (num > 999) {
+    romanNumeralArr[0] = getThousands(romanNumeralArr[0]);
+    romanNumeralArr[1] = getHundreds(romanNumeralArr[1]);
+    romanNumeralArr[2] = getTens(romanNumeralArr[2]);
+    romanNumeralArr[3] = getUnits(romanNumeralArr[3]);
+    convertedNumber = romanNumeralArr.join("");
+    output.innerText = convertedNumber;
+    output.classList.add("output");
+  } else if (num > 99) {
+    romanNumeralArr[0] = getHundreds(romanNumeralArr[0]);
+    romanNumeralArr[1] = getTens(romanNumeralArr[1]);
+    romanNumeralArr[2] = getUnits(romanNumeralArr[2]);
+    convertedNumber = romanNumeralArr.join("");
+    output.innerText = convertedNumber;
+    output.classList.add("output");
+  } else if (num > 9) {
+    romanNumeralArr[0] = getTens(romanNumeralArr[0]);
+    romanNumeralArr[1] = getUnits(romanNumeralArr[1]);
+    convertedNumber = romanNumeralArr.join("");
+    output.innerText = convertedNumber;
+    output.classList.add("output");
+  } else {
+    romanNumeralArr[0] = getUnits(romanNumeralArr[0]);
+    convertedNumber = romanNumeralArr.join("");
+    output.innerText = convertedNumber;
+    output.classList.add("output");
+  }
 };
 
 const getThousands = (num) => {
   switch(num) {
-    case 1:
+    case "1":
       return "M";
-    case 2:
+    case "2":
       return "MM";
-    case 3:
+    case "3":
       return "MMM";
   }
 };
 
 const getHundreds = (num) => {
   switch(num) {
-    case 1:
+    case "1":
       return "C";
-    case 2:
+    case "2":
       return "CC";
-    case 3:
+    case "3":
       return "CCC";
-    case 4:
+    case "4":
       return "CD";
-    case 5:
+    case "5":
       return "D";
-    case 6:
+    case "6":
       return "DC";
-    case 7:
+    case "7":
       return "DCC";
-    case 8:
+    case "8":
       return "DCCC";
-    case 9:
+    case "9":
       return "CM";
   }
 };
 
 const getTens = (num) => {
   switch(num) {
-    case 1:
+    case "1":
       return "X";
-    case 2:
+    case "2":
       return "XX";
-    case 3:
+    case "3":
       return "XXX";
-    case 4:
+    case "4":
       return "XL";
-    case 5:
+    case "5":
       return "L";
-    case 6:
+    case "6":
       return "LX";
-    case 7:
+    case "7":
       return "LXX";
-    case 8:
+    case "8":
       return "LXXX";
-    case 9:
+    case "9":
       return "XC";
   }
 };
 
 const getUnits = (num) => {
   switch(num) {
-    case 1:
+    case "1":
       return "I";
-    case 2:
+    case "2":
       return "II";
-    case 3:
+    case "3":
       return "III";
-    case 4:
+    case "4":
       return "IV";
-    case 5:
+    case "5":
       return "V";
-    case 6:
+    case "6":
       return "VI";
-    case 7:
+    case "7":
       return "VII";
-    case 8:
+    case "8":
       return "VIII";
-    case 9:
+    case "9":
       return "IX";
   }
 };
